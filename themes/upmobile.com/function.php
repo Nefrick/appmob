9b8af5f1670199af78f58ec13b6c650a2fb6473a
Nefrick <it.nefrick@gmail.com>
2019-10-06 19:44:36 +0300

    init commit

---
+++ themes/upmobile.com/function.php
@@ -0,0 +1,7 @@
+<?php
+
+// Set up
+add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-formats', array('aside', 'link', 'image', 'quote', 'video', 'audio') );
